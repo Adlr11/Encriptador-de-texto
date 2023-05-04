0d05d72fef98ecdf93136f615317040405500b33
Alexis <alexis.pr.sk8@gmail.com>
2023-05-03 18:53:33 -0600

    agregando encriptacion de numeros

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ const copiarBtn = document.querySelector("#copiar");
 
 // Función para encriptar el mensaje
 const encriptar = (mensaje) => {
-  const vowels = { a: "ai", e: "enter", i: "imes", o: "ober", u: "ufat" };
+  const vowels = { a: "ai", e: "enter", i: "imes", o: "ober", u: "ufat", 
+  0: "sak", 1: "tpk", 2: "kus", 3: "dopr", 4: "verw", 5: "liq", 6: "ziu", 7: "xlo", 8: "bxoy", 9: "pra"};
   mensaje = mensaje.toLowerCase();
-  return mensaje.replace(/[aeiou]/g, (v) => vowels[v]);
+  return mensaje.replace(/[aeiou0123456789]/g, (v) => vowels[v]);
 };
 
 // Función para desencriptar el mensaje
 const desencriptar = (mensaje) => {
-  const vowels = { ai: "a", enter: "e", imes: "i", ober: "o", ufat: "u" };
+  const vowels = { ai: "a", enter: "e", imes: "i", ober: "o", ufat: "u", 
+  sak: "0", tpk: "1", kus: "2", dopr: "3", verw: "4", liq: "5", ziu: "6", xlo: "7", bxoy: "8", pra: "9" };
   mensaje = mensaje.toLowerCase();
-  return mensaje.replace(/ai|enter|imes|ober|ufat/g, (v) => vowels[v]);
+  return mensaje.replace(/ai|enter|imes|ober|ufat|sak|tpk|kus|dopr|verw|liq|ziu|xlo|bxoy|pra/g, (v) => vowels[v]);
 };
 
 // Función para mostrar el mensaje encriptado/desencriptado
